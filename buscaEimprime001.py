25bd617870990fbcd81b21a0d1fb2577824230d7
antadlp <ant.adlp1986@gmail.com>
2017-03-10 17:16:38 -0600

    algo

--- buscaEimprime001.py
+++ buscaEimprime001.py
@@ -16,10 +16,28 @@ def leeEimprimeZZ(inputFile, outputFile):
    F.close()
    return;
 
+def leeEpasaArregloZZ(inputFile):
+   
+   F = open(inputFile,'r')
 
-      
-leeEimprimeZZ('zz', "ver3.dat")
-leeEimprimeZZ('zz', "ver4.dat")
+   A = []
+   for line in F:
+      if re.search(r'\spotential energy\s',line,re.M|re.I):
+         A.append(float(line.split()[2]))
 
+
+   F.close()
+   return A;
+
+#leeEimprimeZZ('zz', "ver3.dat")
+#leeEimprimeZZ('zz', "ver4.dat")
+
+B = leeEpasaArregloZZ('zz')
+
+for p in B[0:10]:
+   print(p)
+
+
+   
 
 
