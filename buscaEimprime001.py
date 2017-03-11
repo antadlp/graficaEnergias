8938d22663dc47e1c145e3cbfd92818708a5a932
antadlp <ant.adlp1986@gmail.com>
2017-03-10 19:18:53 -0600

    algo

--- buscaEimprime001.py
+++ buscaEimprime001.py
@@ -29,15 +29,45 @@ def leeEpasaArregloZZ(inputFile):
    F.close()
    return A;
 
-#leeEimprimeZZ('zz', "ver3.dat")
-#leeEimprimeZZ('zz', "ver4.dat")
-
-B = leeEpasaArregloZZ('zz')
-
-for p in B[0:10]:
-   print(p)
 
 
+def saveArrayToFile(array, outputFile):
+ 
+   fileOut = open(outputFile, "w")
+
+   for p in array[50:(len(array)-1)]:
+      fileOut.write(str(p))
+      fileOut.write("\n")
+
+   
+   fileOut.close()
+   return;
+
+
+eGP = leeEpasaArregloZZ('../datosMallaPython/zz-GP.dat')
+eBN = leeEpasaArregloZZ('../datosMallaPython/zz-BN.dat')
+eBN23N23 = leeEpasaArregloZZ('../datosMallaPython/zz-B23N23.dat')
+eB2N2 = leeEpasaArregloZZ('../datosMallaPython/zz-B2N2.dat')
+eB3N3 = leeEpasaArregloZZ('../datosMallaPython/zz-B3N3.dat')
+eB6N6 = leeEpasaArregloZZ('../datosMallaPython/zz-B6N6.dat')
+eB12N12a = leeEpasaArregloZZ('../datosMallaPython/zz-B12N12a.dat')
+eB12N12b = leeEpasaArregloZZ('../datosMallaPython/zz-B12N12b.dat')
+eB12N12c = leeEpasaArregloZZ('../datosMallaPython/zz-B12N12c.dat')
+eB12N12d = leeEpasaArregloZZ('../datosMallaPython/zz-B12N12d.dat')
+eB12N12e = leeEpasaArregloZZ('../datosMallaPython/zz-B12N12e.dat')
+
+
+saveArrayToFile(eGP, '../datosMallaPython/eGP.dat')
+saveArrayToFile(eBN, '../datosMallaPython/eBN.dat')
+saveArrayToFile(eBN23N23, '../datosMallaPython/eBN23N23.dat')
+saveArrayToFile(eB2N2, '../datosMallaPython/eB2N2.dat')
+saveArrayToFile(eB3N3, '../datosMallaPython/eB3N3.dat')
+saveArrayToFile(eB6N6, '../datosMallaPython/eB6N6.dat')
+saveArrayToFile(eB12N12a, '../datosMallaPython/eB12N12a.dat')
+saveArrayToFile(eB12N12b, '../datosMallaPython/eB12N12b.dat')
+saveArrayToFile(eB12N12c, '../datosMallaPython/eB12N12c.dat')
+saveArrayToFile(eB12N12d, '../datosMallaPython/eB12N12d.dat')
+saveArrayToFile(eB12N12e, '../datosMallaPython/eB12N12e.dat')
    
 
 
