6b2f242e442942173706f2d15019d343fe84fb26
antadlp <ant.adlp1986@gmail.com>
2017-03-15 18:47:54 -0600

    algo

--- fft-python02.py
+++ fft-python02.py
@@ -11,9 +11,23 @@ y = np.sin(50.0 * 2.0*np.pi*x) + 0.5*np.sin(80.0 * 2.0*np.pi*x)
 yf = scipy.fftpack.fft(y)
 xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
 
+
+for p in yf[0:9]: # it does not reach the yf[9] 
+   print(p)       # i.e. only prints 0,1,..,7,8
+
+
+
+print("\n")
+print(yf[9]) # it was not printed in the for above 
+
+
+
+
 fig, ax = plt.subplots()
 ax.plot(xf, 2.0/N * np.abs(yf[:N//2]))
 plt.show()
+
+
 
 ##
 #Floor Division - The division of operands where the result is the
---
+++ fft1d-Energias01.py
@@ -0,0 +1,78 @@
+import re
+import sys
+from buscaEimprime001 import leeEpasaArregloDAT
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.fftpack
+
+
+
+
+
+# get the values of energy from .dat files which has been
+# obtained from ./buscaEimprime001.py 
+
+eGP = leeEpasaArregloDAT('../datosMallaPython/eGP.dat')
+eBN = leeEpasaArregloDAT('../datosMallaPython/eBN.dat')
+eB23N23 = leeEpasaArregloDAT('../datosMallaPython/eB23N23.dat')
+eB2N2 = leeEpasaArregloDAT('../datosMallaPython/eB2N2.dat')
+eB3N3 = leeEpasaArregloDAT('../datosMallaPython/eB3N3.dat')
+eB6N6 = leeEpasaArregloDAT('../datosMallaPython/eB6N6.dat')
+eB12N12a = leeEpasaArregloDAT('../datosMallaPython/eB12N12a.dat')
+eB12N12b = leeEpasaArregloDAT('../datosMallaPython/eB12N12b.dat')
+eB12N12c = leeEpasaArregloDAT('../datosMallaPython/eB12N12c.dat')
+eB12N12d = leeEpasaArregloDAT('../datosMallaPython/eB12N12d.dat')
+eB12N12e = leeEpasaArregloDAT('../datosMallaPython/eB12N12e.dat')
+
+
+# define x-axis array representing time
+
+x = np.linspace(1.0, 5000, 5000)
+
+
+# compute fft 1-dimensional 
+
+eGPFFTdot = np.multiply(scipy.fftpack.fft(eGP), \
+      np.conjugate(scipy.fftpack.fft(eGP)))
+
+print("\n")
+print('el max eGPFFTdot es %f e INDEX: %d') % \
+np.max(eGPFFTdot), np.where(eGPFFTdot==eGPFFTdot.max())
+print("\n")
+
+# plot time vs energy
+
+fig = plt.figure(0)        
+fig.canvas.set_window_title('time vs Energy GP')
+plt.plot(x, eGP, color='blue', linewidth=1.0, linestyle='-')
+
+# plot time vs fft-energy
+
+fig = plt.figure(1)        
+fig.canvas.set_window_title('time vs Energy FFT GP')
+plt.plot(x, eGPFFTdot, color='blue', linewidth=1.0, linestyle='-')
+
+
+
+
+
+
+plt.show()
+
+# plot time vs fft-energy
+
+## UTILITIES ## Templates for possible functions to use
+#
+#plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
+#plt.plot(x, eGP, 'r--')
+
+
+
+
+
+
+
+
+
+
+
